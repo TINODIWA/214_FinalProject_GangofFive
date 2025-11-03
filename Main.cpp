09a8add768b720dc1643cee677a364eda1cf1e4c
Unathi <babychucks05@gmail.com>
2025-11-03 10:12:11 +0200

    Updated main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,7 @@
 #include "Director.h"
 #include "Builder.h"
+#include "Card.h"
+#include "Cash.h"
 #include "CropBuilder.h"
 #include "PlantBuilder.h"
 #include "Customer.h"
@@ -24,43 +26,39 @@
 #include "BuildPlant.h"
 
 int main() {
-    cout << "\n\nTESTING DECORATOR\n";
-    Staff* baseStaff = new BaseStaff(nullptr, "Nathan");
-    cout << baseStaff->getName() << "\n" << baseStaff->getType() << "\n" << baseStaff->jobDesc() << endl;
+  cout << "\n\nTESTING DECORATOR\n";
+  Staff* baseStaff = new BaseStaff(nullptr, "Nathan");
+  cout << baseStaff->getName() << "\n" << baseStaff->getType() << "\n" << baseStaff->jobDesc() << endl;
 
-    Staff* staff_1 = new Sales(baseStaff);
-    cout << endl;
-    cout << staff_1->getName() << "\n" << staff_1->getType() << "\n" << staff_1->jobDesc() << endl;
+  Staff* staff_1 = new Sales(baseStaff);
+  cout << endl;
+  cout << staff_1->getName() << "\n" << staff_1->getType() << "\n" << staff_1->jobDesc() << endl;
 
-    Staff* baseStaff_2 = new BaseStaff(nullptr, "Unathi");
-    cout << baseStaff_2->getName() << "\n" << baseStaff_2->getType() << "\n" << baseStaff_2->jobDesc() << endl;
+  Staff* baseStaff_2 = new BaseStaff(nullptr, "Unathi");
+  cout << baseStaff_2->getName() << "\n" << baseStaff_2->getType() << "\n" << baseStaff_2->jobDesc() << endl;
 
-    Staff* staff_2 = new Admin(baseStaff_2);
-    cout << endl;
-    cout << staff_2->getName() << "\n" << staff_2->getType() << "\n" << staff_2->jobDesc() << endl;
+  Staff* staff_2 = new Admin(baseStaff_2);
+  cout << endl;
+  cout << staff_2->getName() << "\n" << staff_2->getType() << "\n" << staff_2->jobDesc() << endl;
 
-    Staff* baseStaff_3 = new BaseStaff(nullptr, "Dominiqu");
-    cout << baseStaff_3->getName() << "\n" << baseStaff_3->getType() << "\n" << baseStaff_3->jobDesc() << endl;
+  Staff* baseStaff_3 = new BaseStaff(nullptr, "Dominiqu");
+  cout << baseStaff_3->getName() << "\n" << baseStaff_3->getType() << "\n" << baseStaff_3->jobDesc() << endl;
 
-    Staff* staff_3 = new Management(baseStaff_3);
-    cout << endl;
-    cout << staff_3->getName() << "\n" << staff_3->getType() << "\n" << staff_3->jobDesc() << endl;
+  Staff* staff_3 = new Management(baseStaff_3);
+  cout << endl;
+  cout << staff_3->getName() << "\n" << staff_3->getType() << "\n" << staff_3->jobDesc() << endl;
 
-    Staff* staff_4 = new BaseStaff(nullptr, "Swey");
-    cout << staff_4->getName() << "\n" << staff_4->getType() << "\n" << staff_4->jobDesc() << endl;
+  Staff* staff_4 = new BaseStaff(nullptr, "Swey");
+  cout << staff_4->getName() << "\n" << staff_4->getType() << "\n" << staff_4->jobDesc() << endl;
 
-    Staff* staff_5 = new Gardening(staff_4);
-    cout << endl;
-    cout << staff_5->getName() << "\n" << staff_5->getType() << "\n" << staff_5->jobDesc() << endl;
+  Staff* staff_5 = new Gardening(staff_4);
+  cout << endl;
+  cout << staff_5->getName() << "\n" << staff_5->getType() << "\n" << staff_5->jobDesc() << endl;
 
-    delete staff_5;
-    delete staff_4;
-    delete staff_3;
-    delete staff_2;
-    delete staff_1;
-    delete baseStaff_2;
-    delete baseStaff_3;
-    delete baseStaff;
+    // delete baseStaff_2;
+  // delete baseStaff_3;
+  // delete staff_4;
+  // delete baseStaff;
   cout << "TESTING GARDEN BUILDER\n";
   Builder* cropBuilder = new CropBuilder();
   BuildPlant* plantBuilder = new PlantBuilder();
@@ -71,35 +69,31 @@ int main() {
   cout << "\n\nGarden Created \n";
   garden->print();
 
-  // cout << "TESTING ORDER TEMPLATE METHOD\n";
+  cout << "\n==================TESTING CHAIN\n";
+  Staff* base = new BaseStaff(nullptr, "Joy");
+  Staff* gardener = new Gardening(new BaseStaff(NULL, "Grace"));
+  Staff* sales = new Sales(new BaseStaff(NULL, "Peace"));
+  Staff* manager = new Management(new BaseStaff(NULL, "Love"));
 
-  // Payment* eft = new EFT();
-  // map<Plant*, int> order_1;
 
-  // Plant* acacia = new Tree();
-  // acacia->setName("Acacia");
-  // acacia->setType("Tree");
-  // acacia->setPrice(20);
+  Request req = Request("HIIIII");
 
-  // Plant* lavender = new Shrub();
-  // lavender->setName("Lavender");
-  // lavender->setType("Shrub");
-  // lavender->setPrice(5);
+  Staff* chain = base;
+  base->setSuccessor(gardener);
+  gardener->setSuccessor(sales);
+  sales->setSuccessor(manager);
+  manager->setSuccessor(NULL);
 
-  // order_1[acacia] = 2;
-  // order_1[lavender] = 3;
+  chain->handleCustomer(req);
 
-  // Staff* teller_1 = new Staff("Maggie");
-  // Customer* c1 = new Customer("Dom");
-
-  // string order_receipt = eft->purchase(c1, order_1, teller_1);
-  // cout << order_receipt << endl;
-
-  // delete eft;
-  // delete acacia;
-  // delete lavender;
-  // delete teller_1;
-  // delete c1;
-
-    return 0;
+  cout << "\n===================Memory Management!!!" << endl;
+  delete staff_5;
+  delete staff_3;
+  delete staff_2;
+  delete staff_1;
+  delete base;
+  delete gardener;
+  delete sales;
+  delete manager;
+  return 0;
 }
