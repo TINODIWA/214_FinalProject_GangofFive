b3ce4e1cbc3d72b9ebb6ab638cd397c60dc9dc87
Swe-yy <u24584585@tuks.co.za>
2025-11-03 12:22:26 +0200

    rough idea
    just a rough outline of how the main loop would run

--- Main.cpp
+++ Main.cpp
@@ -36,81 +36,123 @@
 #include "BuildPlant.h"
 
 int main() {
-    cout << "\n\nTESTING DECORATOR\n";
-    Staff* baseStaff = new BaseStaff(nullptr, "Nathan");
-    cout << baseStaff->getName() << "\n" << baseStaff->getType() << "\n" << baseStaff->jobDesc() << endl;
+  // MAIN LOOP IDEA //
+  // Director dir = Director(new CropBuilder(), new PlantBuilder());
 
-    Staff* staff_1 = new Sales(baseStaff);
-    cout << endl;
-    cout << staff_1->getName() << "\n" << staff_1->getType() << "\n" << staff_1->jobDesc() << endl;
+  // GardenPlot* plot = new GardenPlot("plants.txt");
+  // Nursery* staff = new StaffCo_ordination(*plot);
+  // Nursery* customer = new CustomerCare(*plot); //assuming chain of responsibility is instantiated here
 
-    Staff* baseStaff_2 = new BaseStaff(nullptr, "Unathi");
-    cout << baseStaff_2->getName() << "\n" << baseStaff_2->getType() << "\n" << baseStaff_2->jobDesc() << endl;
+  // instantiate staff
 
-    Staff* staff_2 = new Admin(baseStaff_2);
-    cout << endl;
-    cout << staff_2->getName() << "\n" << staff_2->getType() << "\n" << staff_2->jobDesc() << endl;
+  int days = 0;
+  bool sim = true;
+  while (sim) {
+    int numCustomer = rand() % 9;
+    for (int i = 0; i < 8; i++) {
+      // plot.transpire(/*decreasedLevel*/0);
+      // gardener.checkPlant();
+      if (i % numCustomer == 0) {
+        // Customer c = new Customer(name);
+        // customer.attach(c);
+        //  c.makeReq("Enter");
+      }
+    }
+    ++days;
 
-    Staff* baseStaff_3 = new BaseStaff(nullptr, "Dominiqu");
-    cout << baseStaff_3->getName() << "\n" << baseStaff_3->getType() << "\n" << baseStaff_3->jobDesc() << endl;
+    // should we do some restoking here
+    //  iterate through the garden and restock plants.
+    //  we would have to compare to the original parsed file
+    //  maybe some to staff communication chat
 
-    Staff* staff_3 = new Management(baseStaff_3);
-    cout << endl;
-    cout << staff_3->getName() << "\n" << staff_3->getType() << "\n" << staff_3->jobDesc() << endl;
+    // delete plot
+    // delete staff
+    // delete customer
+    // delete instantiated staff
+  }
 
-    Staff* staff_4 = new BaseStaff(nullptr, "Swey");
-    cout << staff_4->getName() << "\n" << staff_4->getType() << "\n" << staff_4->jobDesc() << endl;
 
-    Staff* staff_5 = new Gardening(staff_4);
-    cout << endl;
-    cout << staff_5->getName() << "\n" << staff_5->getType() << "\n" << staff_5->jobDesc() << endl;
 
-    delete staff_5;
-    delete staff_3;
-    delete staff_2;
-    delete staff_1;
-    delete baseStaff_2;
-    delete baseStaff_3;
-    delete baseStaff;
-  cout << "TESTING GARDEN BUILDER\n";
-  Builder* cropBuilder = new CropBuilder();
-  BuildPlant* plantBuilder = new PlantBuilder();
 
-  Director dir = Director(cropBuilder, plantBuilder);
-  Garden* garden = dir.construct("plants.txt");
 
-  cout << "\n\nGarden Created \n";
-  garden->print();
+  
+  // ======================================================================
+  // ======================================================================
+  //   cout << "\n\nTESTING DECORATOR\n";
+  //   Staff* baseStaff = new BaseStaff(nullptr, "Nathan");
+  //   cout << baseStaff->getName() << "\n" << baseStaff->getType() << "\n" << baseStaff->jobDesc() << endl;
 
-  // cout << "TESTING ORDER TEMPLATE METHOD\n";
+  //   Staff* staff_1 = new Sales(baseStaff);
+  //   cout << endl;
+  //   cout << staff_1->getName() << "\n" << staff_1->getType() << "\n" << staff_1->jobDesc() << endl;
 
-  // Payment* eft = new EFT();
-  // map<Plant*, int> order_1;
+  //   Staff* baseStaff_2 = new BaseStaff(nullptr, "Unathi");
+  //   cout << baseStaff_2->getName() << "\n" << baseStaff_2->getType() << "\n" << baseStaff_2->jobDesc() << endl;
 
-  // Plant* acacia = new Tree();
-  // acacia->setName("Acacia");
-  // acacia->setType("Tree");
-  // acacia->setPrice(20);
+  //   Staff* staff_2 = new Admin(baseStaff_2);
+  //   cout << endl;
+  //   cout << staff_2->getName() << "\n" << staff_2->getType() << "\n" << staff_2->jobDesc() << endl;
 
-  // Plant* lavender = new Shrub();
-  // lavender->setName("Lavender");
-  // lavender->setType("Shrub");
-  // lavender->setPrice(5);
+  //   Staff* baseStaff_3 = new BaseStaff(nullptr, "Dominiqu");
+  //   cout << baseStaff_3->getName() << "\n" << baseStaff_3->getType() << "\n" << baseStaff_3->jobDesc() << endl;
 
-  // order_1[acacia] = 2;
-  // order_1[lavender] = 3;
+  //   Staff* staff_3 = new Management(baseStaff_3);
+  //   cout << endl;
+  //   cout << staff_3->getName() << "\n" << staff_3->getType() << "\n" << staff_3->jobDesc() << endl;
 
-  // Staff* teller_1 = new Staff("Maggie");
-  // Customer* c1 = new Customer("Dom");
+  //   Staff* staff_4 = new BaseStaff(nullptr, "Swey");
+  //   cout << staff_4->getName() << "\n" << staff_4->getType() << "\n" << staff_4->jobDesc() << endl;
 
-  // string order_receipt = eft->purchase(c1, order_1, teller_1);
-  // cout << order_receipt << endl;
+  //   Staff* staff_5 = new Gardening(staff_4);
+  //   cout << endl;
+  //   cout << staff_5->getName() << "\n" << staff_5->getType() << "\n" << staff_5->jobDesc() << endl;
 
-  // delete eft;
-  // delete acacia;
-  // delete lavender;
-  // delete teller_1;
-  // delete c1;
+  //   delete staff_5;
+  //   delete staff_3;
+  //   delete staff_2;
+  //   delete staff_1;
+  //   delete baseStaff_2;
+  //   delete baseStaff_3;
+  //   delete baseStaff;
+  // cout << "TESTING GARDEN BUILDER\n";
+  // Builder* cropBuilder = new CropBuilder();
+  // BuildPlant* plantBuilder = new PlantBuilder();
+
+  // Director dir = Director(cropBuilder, plantBuilder);
+  // Garden* garden = dir.construct("plants.txt");
+
+  // cout << "\n\nGarden Created \n";
+  // garden->print();
+
+  // // cout << "TESTING ORDER TEMPLATE METHOD\n";
+
+  // // Payment* eft = new EFT();
+  // // map<Plant*, int> order_1;
+
+  // // Plant* acacia = new Tree();
+  // // acacia->setName("Acacia");
+  // // acacia->setType("Tree");
+  // // acacia->setPrice(20);
+
+  // // Plant* lavender = new Shrub();
+  // // lavender->setName("Lavender");
+  // // lavender->setType("Shrub");
+  // // lavender->setPrice(5);
+
+  // // order_1[acacia] = 2;
+  // // order_1[lavender] = 3;
+
+  // // Staff* teller_1 = new Staff("Maggie");
+  // // Customer* c1 = new Customer("Dom");
+
+  // // string order_receipt = eft->purchase(c1, order_1, teller_1);
+  // // cout << order_receipt << endl;
+
+  // // delete eft;
+  // // delete acacia;
+  // // delete lavender;
+  // // delete teller_1;
+  // // delete c1;
 
   return 0;
 }
